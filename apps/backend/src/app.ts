3f57cc2bf3bd2be2f2d6e7b4172e95e65b4ad81e
Thisan Mindiya Kulathilake <139038281+mindiya12@users.noreply.github.com>
2025-12-07 20:29:22 +0530

    chore: add backend & frontend linting and basic tests

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { pool } from './config/db';
 import taskRoutes from './api/routes/taskRoutes';
 
 dotenv.config();
